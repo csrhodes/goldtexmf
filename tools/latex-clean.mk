497cdefa08b1fc98547e55352e5bffa40ccc0de4
Christophe Rhodes <crhodes@dunstaple.doc.gold.ac.uk>
2009-11-25 17:52:20 +0000

    Makefile infrastructure enhancements
    Generate separate user and internals manuals, with the user manual not
    including the documented code.
    
    Highly-complicated and probably fragile Makefile glue to generate tarballs
    for distribution and release for gsexam.

--- tools/latex-clean.mk
+++ tools/latex-clean.mk
@@ -2,3 +2,4 @@ SHELL=/bin/bash
 
 clean:
 	rm -f $(NAME).{aux,cls,dvi,glo,gls,idx,ilg,ind,log,pdf,ps,sty}
+	rm -f $(NAME)-internals.{aux,dvi,glo,gls,idx,ilg,ind,log,pdf,ps}
--- tools/latex-doc.mk
+++ tools/latex-doc.mk
@@ -1,5 +1,13 @@
 $(NAME).pdf: $(NAME).dtx
-	$(PDFLATEX) $<
+	$(PDFLATEX) "\AtBeginDocument{\OnlyDescription} \input $<"
 	[ -f $(NAME).glo ] && $(MAKEINDEX) -s gglo.ist -o $(NAME).gls $(NAME).glo
 	[ -f $(NAME).idx ] && $(MAKEINDEX) -s gind.ist -o $(NAME).ind $(NAME).idx
-	$(PDFLATEX) $<
+	$(PDFLATEX) "\AtBeginDocument{\OnlyDescription} \input $<"
+
+$(NAME)-internals.pdf: $(NAME).dtx
+	$(PDFLATEX) -jobname $(NAME)-internals $<
+	[ -f $(NAME)-internals.glo ] && $(MAKEINDEX) -s gglo.ist -o $(NAME)-internals.gls $(NAME)-internals.glo
+	[ -f $(NAME)-internals.idx ] && $(MAKEINDEX) -s gind.ist -o $(NAME)-internals.ind $(NAME)-internals.idx
+	$(PDFLATEX) -jobname $(NAME)-internals $<
+
+
