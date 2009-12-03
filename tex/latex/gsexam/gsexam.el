eac8c7d8c88ea25e03ebcc07ab00cad0a2b6870c
Christophe Rhodes <csr21@localhost.localdomain>
2009-12-03 11:20:53 +0000

    marginally better indentation for the contents of answer environments.
    (Do you see what I did there?)
    
    Instead of current-indentation, which is more suited to verbatim, don't
    give a special indentation for |answer| at all.  This allows filling with
    M-q and friends.

--- tex/latex/gsexam/gsexam.el
+++ tex/latex/gsexam/gsexam.el
@@ -3,7 +3,15 @@
  (lambda ()
    (LaTeX-add-environments "bscexam" "question" "answer")
    (make-local-variable 'LaTeX-indent-environment-list)
-   (add-to-list 'LaTeX-indent-environment-list '("answer" current-indentation))
+   ;; The below makes the answer environment a bit like verbatim, in
+   ;; that M-q (`fill-paragraph') will refuse to fill.  Using (lambda
+   ;; () 2) instead of `current-indentation' indents the body of the
+   ;; answer environment correctly, but still refuses to fill on M-q.
+   ;; Without the directive, the indentation of the environments
+   ;; contents is forced to the left (except in nested environments,
+   ;; which add 2 to the indentation level as one might like) but
+   ;; filling works, so let's go with that.
+   ;; (add-to-list 'LaTeX-indent-environment-list '("answer" current-indentation))
    (make-local-variable 'LaTeX-verbatim-regexp)
    (setq LaTeX-verbatim-regexp (concat LaTeX-verbatim-regexp "\\|answer"))
    (add-to-list 'LaTeX-verbatim-environments-local "answer")
